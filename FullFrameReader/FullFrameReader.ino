0729d5e3d2680c9cb75f587a87973f017d6f846f
Reece Bell <rrb32@bath.ac.uk>
2022-03-08 16:09:15 +0000

    All files on uni pcs pulled

--- FullFrameReader/FullFrameReader.ino
+++ FullFrameReader/FullFrameReader.ino
@@ -1,22 +1,29 @@
-/*
-    MPU6050 Triple Axis Gyroscope & Accelerometer. Simple Gyroscope Example.
-    Read more: http://www.jarzebski.pl/arduino/czujniki-i-sensory/3-osiowy-zyroskop-i-akcelerometr-mpu6050.html
-    GIT: https://github.com/jarzebski/Arduino-MPU6050
-    Web: http://www.jarzebski.pl
-    (c) 2014 by Korneliusz Jarzebski
-*/
+// script to read full data frame from IMU and PR matrix
+
 
 #include <Wire.h>
 #include <MPU6050.h>
 
 MPU6050 mpu;
 
+//Set column pins to be energise by arduino
+int ColOne = 52;
+int ColTwo = 50;
+int ColThree = 48;
+int ColFour = 46;
+
+//Set Analogue pins for reading data
+int RowOne = A8;
+int RowTwo = A9;
+int RowThree = A10;
+int RowFour = A11;
+
+unsigned long MyTime;
+
 void setup() 
 {
-  //Serial.begin(2000000); // BAUD RATE IS NOT LIMITING FACTOR
-  
   Serial.begin(115200);
-  
+
   // Initialize MPU6050
   Serial.println("Initialize MPU6050");
   while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
@@ -40,6 +47,19 @@ void setup()
   
   // Check settings
   checkSettings();
+
+  // Set up the row scanner pinmodes and initialise all low
+  //Set digital pins to output mode
+  pinMode(ColOne,OUTPUT);
+  pinMode(ColTwo,OUTPUT);
+  pinMode(ColThree,OUTPUT);
+  pinMode(ColFour,OUTPUT);
+  
+  //Set all pins low to begin
+  digitalWrite(ColOne,LOW);
+  digitalWrite(ColTwo,LOW);
+  digitalWrite(ColThree,LOW);
+  digitalWrite(ColFour,LOW);
 }
 
 void checkSettings()
@@ -82,7 +102,7 @@ void checkSettings()
 
 void loop()
 {
-  int MyTime = millis();
+  MyTime = millis();
   Vector rawGyro = mpu.readRawGyro();
   Vector normGyro = mpu.readNormalizeGyro();
 
@@ -98,10 +118,78 @@ void loop()
   Serial.print(" Ynorm = ");
   Serial.print(normGyro.YAxis);
   Serial.print(" Znorm = ");
-  Serial.print(normGyro.ZAxis);
+  Serial.println(normGyro.ZAxis);
 
-  Serial.print(" Time = ");
-  Serial.println(MyTime);
-  //Serial.println("");
+  // Begin scanning each pixel in series, column by column
+
+  //Column one
+  digitalWrite(ColOne,HIGH);
+  int p11 = analogRead(RowOne);
+  int p12 = analogRead(RowTwo);
+  int p13 = analogRead(RowThree);
+  int p14 = analogRead(RowFour);
+  digitalWrite(ColOne,LOW);
+  
+  //Column two
+  digitalWrite(ColTwo,HIGH);
+  int p21 = analogRead(RowOne);
+  int p22 = analogRead(RowTwo);
+  int p23 = analogRead(RowThree);
+  int p24 = analogRead(RowFour);
+  digitalWrite(ColTwo,LOW);
+  
+  //Column three
+  digitalWrite(ColThree,HIGH);
+  int p31 = analogRead(RowOne);
+  int p32 = analogRead(RowTwo);
+  int p33 = analogRead(RowThree);
+  int p34 = analogRead(RowFour);
+  digitalWrite(ColThree,LOW);
+  
+  //Column four
+  digitalWrite(ColFour,HIGH);
+  int p41 = analogRead(RowOne);
+  int p42 = analogRead(RowTwo);
+  int p43 = analogRead(RowThree);
+  int p44 = analogRead(RowFour);
+  digitalWrite(ColFour,LOW);
+  
+  Serial.print(p11);
+  Serial.print(" ");
+  Serial.print(p12) ;
+  Serial.print(" ");
+  Serial.print(p13) ;
+  Serial.print(" ");
+  Serial.println(p14);
   //delay(1000);
+  
+  Serial.print(p21);
+  Serial.print(" ");
+  Serial.print(p22) ;
+  Serial.print(" ");
+  Serial.print(p23) ;
+  Serial.print(" ");
+  Serial.println(p24) ;
+  //delay(1000);
+  
+  Serial.print(p31);
+  Serial.print(" ");
+  Serial.print(p32) ;
+  Serial.print(" ");
+  Serial.print(p33) ;
+  Serial.print(" ");
+  Serial.println(p34);
+  //delay(1000);
+  
+  Serial.print(p41);
+  Serial.print(" ");
+  Serial.print(p42) ;
+  Serial.print(" ");
+  Serial.print(p43) ;
+  Serial.print(" ");
+  Serial.println(p44) ;
+  
+  Serial.println(MyTime);
+  Serial.println("");
+  delay(1000);
 }
--- TestMatrix/TestMatrix.ino
+++ TestMatrix/TestMatrix.ino
@@ -7,22 +7,25 @@ void setup() {
   Serial.begin(9600);
   pinMode(50, OUTPUT);
   pinMode(52, OUTPUT);
-digitalWrite(50, LOW);
-digitalWrite(52,LOW);
-delay(5000);
+//digitalWrite(50, LOW);
+//digitalWrite(52,LOW);
+//delay(5000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int OneOne = analogRead(A8);
-  Serial.print("Low ");
-  Serial.println(OneOne);
-  delay(1000);
+  //int OneOne = analogRead(A8);
+  //Serial.print("Low ");
+  //Serial.println(OneOne);
+  //delay(1000);
   digitalWrite(50,HIGH);
   digitalWrite(52,HIGH);
-  OneOne = analogRead(A8);
-  Serial.print("High ");  
-  Serial.println(OneOne);
+  int RowOne = analogRead(A8);
+  int RowTwo = analogRead(A9);
+  Serial.print("Row one ");  
+  Serial.println(RowOne);
+  Serial.print("Row two ");  
+  Serial.println(RowTwo);
   delay(1000);
 //  digitalWrite(50,LOW);
 //  digitalWrite(52,LOW); 
